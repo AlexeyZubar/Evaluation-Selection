cf70b32e40303abf877714be2e99dac3eb019b07
AlexeyZubar <lesha9515@gmail.com>
2022-05-09 02:49:08 +0300

    Add nested cross-validation to train

--- train.py
+++ train.py
@@ -12,6 +12,8 @@ from sklearn.preprocessing import MinMaxScaler
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_validate
 
 from .data import get_dataset
 
@@ -117,26 +119,65 @@ def train(
             features_train = StandardScaler().fit_transform(features_train)
         elif feature_engineering=='min_max_scaler': 
             features_train = MinMaxScaler().fit_transform(features_train)
-        cv = KFold(n_splits=5)
-        if(select_model=='logist_regression'):
-            model=LogisticRegression(random_state=random_state, max_iter=max_iter, C=logreg_c,n_jobs=-1)
-        elif(select_model=='random_forest'):
-            model=RandomForestClassifier(random_state=random_state,max_depth=max_depth,max_features=max_features,n_estimators=n_estimators,n_jobs=-1)
-        accuracy = (cross_val_score(model, features_train, target_train, cv = cv, scoring='accuracy')).mean()
-        f1_micro = (cross_val_score(model, features_train, target_train, cv = cv, scoring='f1_micro')).mean()
-        roc_auc_ovr = (cross_val_score(model, features_train, target_train, cv = cv, scoring='roc_auc_ovr')).mean()
-        mlflow.log_param("feature_engineering",feature_engineering)
-        mlflow.log_param("max_iter", max_iter)
-        mlflow.log_param("logreg_c", logreg_c)
-        mlflow.log_param("select_model", select_model)
-        mlflow.log_param("max_depth", max_depth)
-        mlflow.log_param("max_features", max_features)
-        mlflow.log_param("n_estimators", n_estimators)
+        cv = KFold(n_splits=8, shuffle=True, random_state=random_state)
+        
+        if search=='KFold':
+            if(select_model=='logist_regression'):
+                model=LogisticRegression(random_state=random_state, max_iter=max_iter, C=logreg_c,n_jobs=-1)
+            elif(select_model=='random_forest'):
+                model=RandomForestClassifier(random_state=random_state,max_depth=max_depth,max_features=max_features,n_estimators=n_estimators,n_jobs=-1)
+            scores = cross_validate(model, features_train, target_train, cv = cv, scoring=('accuracy', 'f1_macro','roc_auc_ovr'))
+            accuracy=scores['test_accuracy'].mean()
+            f1_macro =scores['test_f1_macro'].mean()
+            roc_auc_ovr =scores['test_roc_auc_ovr'].mean()
+            
+            if(select_model=='logist_regression'):
+                mlflow.log_param("max_iter", max_iter)
+                mlflow.log_param("logreg_c", logreg_c)
+            elif(select_model=='random_forest'):
+                mlflow.log_param("max_depth", max_depth)
+                mlflow.log_param("max_features", max_features)
+                mlflow.log_param("n_estimators", n_estimators)
+        elif search=='NestedCV':
+            if(select_model=='logist_regression'):
+                model=LogisticRegression(random_state=random_state,n_jobs=-1)
+                space = dict()
+                space['max_iter'] = [10, 100,200,400, 500,700]
+                space['C'] = [0.001,0.01, 0.1, 1,10]
+            elif(select_model=='random_forest'):
+                model=RandomForestClassifier(random_state=random_state,n_jobs=-1)
+                space = dict()
+                space['max_depth'] = [None, 5,10,50,70]
+                space['max_features'] = [0.2,0.4, 0.7,1.0]
+                space['n_estimators'] = [20,50, 100, 200]
+                #poetry run train --select-model random_forest
+                #space['max_depth'] = [None, 5]
+                #space['max_features'] = [0.7,1.0]
+                #space['n_estimators'] = [20, 100]
+            search_q = GridSearchCV(model, space, scoring='accuracy', n_jobs=-1, cv=cv)
+            cv_outer = KFold(n_splits=5)
+            scores = cross_validate(search_q, features_train, target_train, cv = cv_outer, scoring=('accuracy', 'f1_macro','roc_auc_ovr'))
+            accuracy=scores['test_accuracy'].mean()
+            f1_macro =scores['test_f1_macro'].mean()
+            roc_auc_ovr =scores['test_roc_auc_ovr'].mean()
+            
+            search_q.fit(features_train,target_train)
+            print('The parameters combination that would give best accuracy is : ',search_q.best_params_)
+            if(select_model=='logist_regression'):
+                mlflow.log_param("max_iter", search_q.best_params_['max_iter'])
+                mlflow.log_param("logreg_c", search_q.best_params_['C'])
+            elif(select_model=='random_forest'):
+                mlflow.log_param("max_depth", search_q.best_params_['max_depth'])
+                mlflow.log_param("max_features", search_q.best_params_['max_features'])
+                mlflow.log_param("n_estimators", search_q.best_params_['n_estimators'])
+        mlflow.log_param("feature_engineering",feature_engineering)        
+        mlflow.log_param("select_model", select_model)        
+        mlflow.log_param("search", search)
         mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("f1_micro", f1_micro)
+        mlflow.log_metric("f1_macro", f1_macro)
         mlflow.log_metric("roc_auc_ovr", roc_auc_ovr)
         click.echo(f"Accuracy: {accuracy}.")
-        click.echo(f"f1_micro: {f1_micro}.")
+        click.echo(f"f1_macro: {f1_macro}.")
         click.echo(f"roc_auc_ovr: {roc_auc_ovr}.")
         click.echo(f"select_model: {select_model}.")
         dump(model, save_model_path)
